432cb7ed5a0cfaa53742039c8fce26462e7437ab
System <none>
2017-03-05 18:04:35 +0000

    ☠️🎉 Updated with Gomix

--- server.js
+++ server.js
@@ -2,36 +2,51 @@
 // where your node app starts
 
 // init project
+var path = require("path");
 var express = require('express');
+var moment = require('moment')
+
 var app = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// homepage
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// if there is string in url 
+app.get('/:timeString', function(request, response){
+  var timeString = request.params.timeString;
+  var output;
+  
+  //check for format 
+   if(/^-?[0-9]*$/.test(timeString)){
+     output = moment(timeString, "X")
+   
+   }
+else{
+  output = moment(timeString, "MMMM DD YYYY");
+}
+  //using moment documentation
+    if (output.isValid()){
+    response.json({
+      unix: output.utc().format("X"),
+      natural: output.utc().format("MMMM D, YYYY")
+    });
+  } else{
+    response.json({
+      unix: 'null',
+      natural: 'null'
+    });
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
